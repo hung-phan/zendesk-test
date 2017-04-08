b793fd64be3183e0a98d592961f5546178d49198
hung-phan <colorvisavn@gmail.com>
2017-04-08 23:11:59 +0700

    Update AddProject

--- ui-question/app/client/components/task-manager/AddProject.jsx
+++ ui-question/app/client/components/task-manager/AddProject.jsx
@@ -4,7 +4,7 @@ import style from './style.scss';
 
 export default class AddProject extends React.PureComponent {
   state: { input: string } = {
-    input: '',
+    input: ''
   };
 
   onChange = (e: Object) => this.setState({ input: e.target.value });
@@ -25,7 +25,7 @@ export default class AddProject extends React.PureComponent {
       <div className={style.AddProject}>
         <h4>add project</h4>
         <input
-          type="text"
+          type='text'
           onKeyUp={this.onKeyUp}
           onChange={this.onChange}
           value={this.state.input}
